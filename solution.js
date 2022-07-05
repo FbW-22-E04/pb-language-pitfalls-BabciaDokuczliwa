6de9843e51ae58ad0ff507c1a50bc9f72e615903
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-07-05 14:21:32 +0200

    pitfalls

--- solution.js
+++ solution.js
@@ -1,1 +1,15 @@
 
+/* Question: Comment your answer in your js file: What does NaN stand for?
+Answer: NaN is information that the checked variable is not a number */
+
+console.log(`Is hello is not a number?`, isNaN(`hello`));
+
+console.log(`What is the type of NaN?`, typeof NaN);
+
+/* A  method to make sure that only 0.02 is printed to the Terminal */
+
+console.log((0.1 * 0.2).toFixed(2));
+
+console.log(`Divide Infinity by 0 is`, Infinity / 0);
+console.log( `Divide Infinity by Infinity is`, Infinity / Infinity);
+console.log(`Divide 1 by 0 is`, 1 / 0);
